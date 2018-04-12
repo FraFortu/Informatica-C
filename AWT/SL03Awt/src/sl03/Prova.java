7117f809134770c4283cc92d0bcc3c8aa3b5368b
FraFortu <francescof2800@gmail.com>
2018-04-12 22:37:54 +0200

    Java projects

---
+++ AWT/SL03Awt/src/sl03/Prova.java
@@ -0,0 +1,17 @@
+package sl03;
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Prova {
+
+	public static void main(String[] args) {
+		new ListaGrafica().create(640, 320, 3, 3);		
+	}
+	
+}
